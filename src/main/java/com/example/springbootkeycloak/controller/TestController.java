5d9609b7f870d00bf24cdcab1632f9004597a60d
bysskc@mail.ru <GitBonaqua15>
2020-08-12 10:16:39 +0600

    return to approach from first commit(turn off Spring Security, secure via application.properties), use keycloak-admin-client to get access token

--- src/main/java/com/example/springbootkeycloak/controller/TestController.java
+++ src/main/java/com/example/springbootkeycloak/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.example.springbootkeycloak.controller;
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.AccessTokenResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,26 @@ public class TestController {
     @GetMapping(path = "/greeting")
     public String greeting() {
         return "Hi :)";
+    }
+
+    @GetMapping(path = "/getToken")
+    public Object getToken() {
+        return getTokenFromKeyCloak();
+    }
+
+    private Object getTokenFromKeyCloak() {
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl("http://localhost:8180/auth")
+                .grantType(OAuth2Constants.PASSWORD)
+                .realm("SpringBootKeycloak")
+                .clientId("key-cloak-client")
+                .username("user1")
+                .password("password")
+                .resteasyClient(
+                        new ResteasyClientBuilder()
+                                .connectionPoolSize(10).build()
+                ).build();
+        AccessTokenResponse accessToken = keycloak.tokenManager().getAccessToken();
+        return accessToken;
     }
 }
